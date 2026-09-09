c7dd8cdfc20373dbafa5058d8ad9993efa32ada2
Aman <amandangi202300@gmail.com>
2026-09-09 11:06:08 +0530

    divide

---
+++ divide5.js
@@ -0,0 +1,10 @@
+let num =Number(prompt("Enter any number "));
+
+if(num%5==0)
+{
+    console.log("It is divisebal by 5  " + num);
+}
+else
+{
+    console.log("It is not divisebal by 5  " + num);
+}
