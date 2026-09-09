ac54778ffaec1801aadde297ff6a57fb75809d8e
Aman <amandangi202300@gmail.com>
2026-09-09 11:20:58 +0530

    divide 3 or 5

---
+++ divide3or5.js
@@ -0,0 +1,11 @@
+let num = Number(prompt("Enter the any number"));
+
+if(num%3==0&&num%5==0)
+{
+    console.log("yes divisebal by 3 or 5  "+ num);
+
+}
+else 
+{
+    console.log("not divisebal by 3 or 5  "+ num);
+}
