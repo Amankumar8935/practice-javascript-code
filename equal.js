700b216ab1b1b53d14ecea362d82b4cc9bc8343c
Aman <amandangi202300@gmail.com>
2026-09-09 10:51:03 +0530

    equal no or not

---
+++ equal.js
@@ -0,0 +1,12 @@
+let num = Number(prompt("Enter first number and second "));
+let num1 = Number(prompt("Enter first number and second "));
+
+if(num==num1)
+{
+    console.log("All are equal  "+num+" "+num1);
+
+}
+else 
+{
+    console.log("All are not equal  "+num+" "+num1);
+}
