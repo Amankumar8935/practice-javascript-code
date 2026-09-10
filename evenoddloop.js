e3818e13fb02b0fdc5ea35e1679468db50870b54
Aman <amandangi202300@gmail.com>
2026-09-10 16:47:11 +0530

    evenoddloop

---
+++ evenoddloop.js
@@ -0,0 +1,15 @@
+console.log("Even Number");
+let num=100;
+let i;
+for(i=0;i<=num;i=i+2)
+{
+    console.log(i);
+}
+console.log("Odd Number");
+
+let odd =100;
+let o;
+for(o=1;o<=odd;o=o+2)
+{
+    console.log(o);
+}
