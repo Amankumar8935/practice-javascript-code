c8c15b2bffc1d361d284bff45b125497a7c78c1f
Aman <amandangi202300@gmail.com>
2026-09-10 16:49:51 +0530

    loop1to10

---
+++ loop1to10.js
@@ -0,0 +1,23 @@
+let num = 10;
+let i;
+for (i=1;i<=num;i++)
+{
+    console.log(i);
+}
+
+//Print numbers from 10 to 1.
+
+console.log("Reverse order");
+ let n=1;
+ let a;
+ for(a=10;a>=n;a--)
+ {
+    console.log(a);
+ }
+console.log("Print 1 to 100");
+ let a1=100;
+ let j;
+ for(j=1;j<=a1;j++)
+ {
+    console.log(j);
+ }
